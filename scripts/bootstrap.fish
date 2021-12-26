01a30c35785d4da7937ec0d0642ab74991b587a1
cacarico <caio.quinilato@gmail.com>
2021-12-26 15:54:33 +0100

    Moving bootstrap to fish

---
+++ scripts/bootstrap.fish
@@ -0,0 +1,26 @@
+#!/usr/bin/env fish
+
+function create_links
+  ln -s $PWD/config/tmux.conf $HOME/.tmux.conf
+  ln -s $PWD/config/fish/config.fish $HOME/.config/fish/config.fish
+  ln -s $PWD/config/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+  ln -s $PWD/config/nvim/init.vim $HOME/.config/nvim/init.vim
+  ln -s $PWD/config/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
+end
+
+function remove_links
+  unlink $HOME/.tmux.conf
+  unlink $HOME/.config/fish/config.fish
+  unlink $HOME/.config/alacritty/alacritty.yml
+  unlink $HOME/.config/nvim/init.vim
+  unlink $HOME/.config/nvim/coc-settings.json
+end
+
+switch $argv
+  case create_links
+    create_links
+  case remove_links
+    remove_links
+  case '*'
+    echo Option $argv not found
+end
--- scripts/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -s $PWD/config/tmux.conf $HOME/.tmux.conf
-ln -s $PWD/config/fish/config.fish $HOME/.config/fish/config.fish
-ln -s $PWD/config/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-ln -s $PWD/config/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -s $PWD/config/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
