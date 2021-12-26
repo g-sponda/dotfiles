991b9e894e885dc7431fb6b8578776317b26edb1
cacarico <caio.quinilato@gmail.com>
2021-12-26 14:22:47 +0100

    Adding terraform for coc

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,4 +3,5 @@
 ln -s $PWD/config/tmux.conf $HOME/.tmux.conf
 ln -s $PWD/config/fish/config.fish $HOME/.config/fish/config.fish
 ln -s $PWD/config/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-ln -s $PWD/config/init.vim $HOME/.config/nvim/init.vim
+ln -s $PWD/config/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -s $PWD/config/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
