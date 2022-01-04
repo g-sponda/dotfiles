536f73aff30f636ad8963cc4026b424f33b8292e
Caio Teixeira <caio.teixeira@aroundhome.de>
2022-01-04 09:10:22 +0100

    Fixing bootstrap script

--- scripts/bootstrap.fish
+++ scripts/bootstrap.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
-set CONFIG_DIR
-set FISH_CONFIG_DIR config/fish
+set CONFIG_DIR $PWD/config
+set FISH_CONFIG_DIR $CONFIG_DIR/fish
 set HOME_CONFIG_DIR $HOME/.config
 set FISH_HOME_CONFIG_DIR $HOME_CONFIG_DIR/fish/conf.d
 
@@ -13,24 +13,24 @@ function create_links
    ln -s $fish_file $FISH_HOME_CONFIG_DIR/
   end
 
-  ln -s config/tmux.conf $HOME/.tmux.conf
-  ln -s config/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-  ln -s config/nvim/init.vim $HOME/.config/nvim/init.vim
-  ln -s config/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
+  ln -s $CONFIG_DIR/tmux.conf $HOME/.tmux.conf
+  ln -s $CONFIG_DIR/alacritty.yml $HOME_CONFIG_DIR/alacritty/alacritty.yml
+  ln -s $CONFIG_DIR/nvim/init.vim $HOME_CONFIG_DIR/nvim/init.vim
+  ln -s $CONFIG_DIR/nvim/coc-settings.json $HOME_CONFIG_DIR/nvim/coc-settings.json
 end
 
 function remove_links
 
   # Fish
   unlink $HOME/.config/fish/config.fish
-  for fish_file in $FISH_CONFIG_DIR/conf.d/*
-    unlink $HOME/.$fish_file
+  for fish_file in (ls $FISH_CONFIG_DIR/conf.d)
+    unlink $FISH_HOME_CONFIG_DIR/$fish_file
   end
 
   unlink $HOME/.tmux.conf
-  unlink $HOME/.config/alacritty/alacritty.yml
-  unlink $HOME/.config/nvim/init.vim
-  unlink $HOME/.config/nvim/coc-settings.json
+  unlink $HOME_CONFIG_DIR/alacritty/alacritty.yml
+  unlink $HOME_CONFIG_DIR/nvim/init.vim
+  unlink $HOME_CONFIG_DIR/nvim/coc-settings.json
 end
 
 switch $argv
